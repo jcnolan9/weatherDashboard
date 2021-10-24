cecb93ffb8cc94624b66070eaba81a60d6dcc5f6
Mbaku Starcrush <j.c.nolan9@gmail.com>
2021-10-23 18:21:10 -0700

    adding comments and removing console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,24 +22,26 @@ var pastSection = document.querySelector("#past")
 var pastSearch = []
 var fetchCount = 0
 
+//grab today's forecast
 function extractCurrentInfo(today) {
+    //extract all the data from the object 
     date = moment(today.dt, "X").format("MMM Do YYYY") 
-    console.log("Today: " + today.dt)
-    console.log(date)
+    
     temperature = today.temp
-    console.log(temperature)
+    
     humidity = today.humidity
-    console.log(humidity)
+    
     windspeed = today.wind_speed
-    console.log(windspeed)
+    
     uvi = today.uvi
-    console.log(uvi)
+    
     current_icon_desc = today.weather[0].icon
     current_icon_link = "http://openweathermap.org/img/wn/" + current_icon_desc + "@2x.png"
-    console.log(current_icon_link)
+    
 
     removeOldInfo()
 
+    //create necessary page elements for the today section 
     var currentHeader = document.createElement("h2")
     currentHeader.textContent = city_returned + " " + "(" + date + ")"
     todaySection.append(currentHeader)
@@ -60,13 +62,25 @@ function extractCurrentInfo(today) {
     humidText.textContent = "Humidity: " + humidity + " %"
     todaySection.append(humidText)
 
+    var uvSpan = document.createElement("span")
     var uvText = document.createElement("p")
     uvText.textContent = "UV Index: " + uvi
-    todaySection.append(uvText)
+    todaySection.append(uvSpan)
+    uvSpan.append(uvText)
+    if(uvi <= 2) {
+        uvSpan.setAttribute("class", "low-uvi")
+    }
+    else if(uvi > 2 && uvi <= 5) {
+        uvSpan.setAttribute("class", "med-uvi")
+    }
+    else {
+        uvSpan.setAttribute("class", "high-uvi")
+    }
 
     todaySection.style.padding = "1%"
 }
 
+//grab the future 5 day forecast
 function extractFutureInfo(future) {
     var fiveDayArray = future.slice(1,6)
     
@@ -93,26 +107,27 @@ function extractFutureInfo(future) {
     var windFutureText
     var humidFutureText
 
+    //loop through the objects for each of the 5 days of the future forecast
     for(var i = 0; i < fiveDayArray.length; i++) {
         futureDate = moment(fiveDayArray[i].dt, "X").format("MMM Do YYYY")
-        console.log(moment(fiveDayArray[i].dt, "X").format("MMM Do YYYY"))
+       
         
         tempMax = fiveDayArray[i].temp.max
-        console.log(fiveDayArray[i].temp.max)
+    
         tempMin = fiveDayArray[i].temp.min
-        console.log(fiveDayArray[i].temp.min)
+        
         
         futureWindspeed = fiveDayArray[i].wind_speed
-        console.log(fiveDayArray[i].wind_speed)
+        
         
         futureHumidity = fiveDayArray[i].humidity
-        console.log(fiveDayArray[i].humidity)
+        
 
-        console.log(fiveDayArray[i])
+       
         futureIconDesc = fiveDayArray[i].weather[0].icon
-        // console.log(futureIconDesc)
         futureIconLink = "http://openweathermap.org/img/wn/" + futureIconDesc + "@2x.png"
         
+        //create all the needed elements 
         daySection = document.createElement("section")
         daySection.setAttribute("id", "day-" + i)
         weatherDiv.append(daySection)
@@ -140,11 +155,10 @@ function extractFutureInfo(future) {
         humidFutureText = document.createElement("p")
         humidFutureText.textContent = "Humidity: " + futureHumidity + " %"
         daySection.append(humidFutureText)
-        
-        console.log("i = " + i)
     }
 }
 
+//use the long and lat coordinates to get the forecasts 
 function getWeather() {
     var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=95ebbf75a9f3b9334688363d6e7930b7"
     fetch(weatherURL)
@@ -152,11 +166,8 @@ function getWeather() {
             return weatherResponse.json()
         })
         .then(function (weatherData) {
-            console.log(weatherData)
             current = weatherData.current
-            console.log(current)
             daily = weatherData.daily
-            console.log(daily)
             extractCurrentInfo(current)
             extractFutureInfo(daily)
             fetchCount++
@@ -164,6 +175,7 @@ function getWeather() {
     
 }
 
+//if it's not the first fetch then remove the old content in the section 
 function removeOldInfo () {
     if(fetchCount > 0) {
         while(todaySection.firstChild) {
@@ -175,6 +187,8 @@ function removeOldInfo () {
     }
 }
 
+//make the actual API call to grab the coordinates of the city and use those to make another call to get the multi-day forecast
+//if the city has not already been searched add it to the pastSearches array and create an element for it on screen 
 function apiFetch () {
     var coordURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=95ebbf75a9f3b9334688363d6e7930b7"
 
@@ -183,13 +197,9 @@ function apiFetch () {
         return response.json()
     })
     .then(function (data) {
-        console.log(data)
         lat = data.coord.lat
         lon = data.coord.lon
-        console.log("Lat: " + lat)
-        console.log("Lon: " + lon)
         city_returned = data.name
-        console.log(city_returned)
         getWeather()
         if(!pastSearch.includes(city_returned)) {
             pastSearch.push(city_returned)
@@ -197,58 +207,41 @@ function apiFetch () {
             var newCity = document.createElement("p")
             newCity.textContent = city_returned
             pastSection.append(newCity)
+            newCity.setAttribute("class", "past-searches")
         }
     })
 }
 
-
+//Adding an event listener to the submit button and kicking of the api requests when submit button pressed
 document.querySelector("#submit").addEventListener("click", function (e) {
     e.preventDefault()
-    console.log("clicked")
     cityInput = textInput.value
     apiFetch() 
 })
 
+//grabbing anything in local storage
 var pastSearchArrayLocal = JSON.parse(localStorage.getItem("cities"))
 
 if(pastSearchArrayLocal == null) {
     pastSearch = []
 }
+//if there is stuff in local storage loop through the array and set the pastCity array equal to what's in local storage
+//create elements on the page with the names of the local sotrage items and add event listeners to them 
 else {
     pastSearch = pastSearchArrayLocal
-    console.log("Not null")
 
     for(var i = 0; i < pastSearchArrayLocal.length; i++) {
         var pastCity = document.createElement("p")
         pastCity.textContent = pastSearchArrayLocal[i]
         pastSection.append(pastCity)
+        pastCity.setAttribute("class", "past-searches")
     }
 
     document.querySelector("#past").addEventListener("click", function (event) {
         var oldCity = event.target
-        console.log(oldCity)
-        cityInput = oldCity.textContent
-        console.log(cityInput)
+        if(event.target != pastSection) {
+            cityInput = oldCity.textContent
         apiFetch()
+        }
     })
 }
-
-
-
-
-
-
-
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-*/
